cbe1918bca9fc16607608d5a4123478dcfda393a
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-06-29 18:55:44 +0100

    UPDATE: auth route

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,5 +1,9 @@
-const auth = () => {
+import express from 'express';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
-}
+const router = express.Router();
 
-export default auth;
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+export default router;
