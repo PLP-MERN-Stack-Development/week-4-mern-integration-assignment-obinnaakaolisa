55e28f011ccdb0973b61a23e0c3b52eac8efb1a1
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-07-01 16:54:35 +0100

    ADD: Request validation for user authentication

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { loginUser, registerUser } from '../controllers/authController.js';
+import { validateRequest } from '../middlewares/validateRequest.js';
+import { registerValidationRules, loginValidationRules } from '../middlewares/authValidators.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', registerValidationRules(), validateRequest, registerUser);
+router.post('/login', loginValidationRules(), validateRequest, loginUser);
 
 export default router;
