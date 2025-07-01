b724208f1e01de411005b907e3620a64c496ce14
obinnaakaolisa <akaolisaobinna@gmail.com>
2025-07-01 16:17:59 +0100

    UPDATE: errorMiddleware.js

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -1,13 +1,19 @@
 export const notFound = (req, res, next) => {
-  res.status(404).json({ message: `Not Found - ${req.originalUrl}` });
+  res.status(404).json({
+    message: `Not Found - ${req.originalUrl}`,
+  });
 };
 
 export const errorHandler = (err, req, res, next) => {
-  res.status(res.statusCode || 500).json({
-    message: err.message || 'Internal Server Error',
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  console.error(err.stack);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
+
 
 export const logErrors = (err, req, res, next) => {
   console.error(err.stack);
